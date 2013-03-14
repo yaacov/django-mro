f2b03f1f45eda58a9eccceba660604eb92df7aac
Yaacov <kobi.zamir@gmail.com>
2013-03-14 15:08:43 +0200

    cleanup

---
+++ mro_warehouse/autocomplete_light_registry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+# Copyright (C) 2013 Yaacov Zamir <kobi.zamir@gmail.com>
+# Author: Yaacov Zamir (2013) <kobi.zamir@gmail.com>
+import autocomplete_light
+
+from mro_warehouse.models import Item
+
+autocomplete_light.register(Item, search_fields=('id', 'name'),
+    autocomplete_js_attributes={'placeholder': 'item name ..'})
